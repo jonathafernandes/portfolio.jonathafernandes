eb6eff9760dcf8317642f536428fd664c004c46e
jonathafernandes <jonathafernandes@live.com>
2023-02-08 12:49:46 -0300

    ajustes no menu responsivo

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Menu responsivo
-
 let show = true
 
 const menuSection = document.querySelector(".menu-section")
@@ -7,14 +6,23 @@ const menuToggle = menuSection.querySelector(".menu-toggle")
 
 menuToggle.addEventListener("click", () => {
     document.body.style.overflow = show ? "hidden" : "initial"
-    document.querySelector(".back-to-top").style.display = show ? "none" : "initial"
 
     menuSection.classList.toggle("on", show)
     show = !show;
 })
 
+document.querySelectorAll('.buttonOption').forEach(button => {
+    button.addEventListener('click', function () {
+        document.querySelectorAll('.menu-section.on').forEach(selectedButton => {
+            selectedButton.classList.remove('on');
+            document.body.style.overflow = "initial"
+        });
+        button.classList.add('on');
+    });
+});
+
 //Botão para voltar ao topo
-var btn = document.querySelector(".back-to-top");
+const btn = document.querySelector(".back-to-top");
 
 btn.addEventListener("click", function () {
     window.scroll({
