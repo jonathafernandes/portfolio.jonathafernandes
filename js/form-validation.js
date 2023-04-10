3ed49daea94075335b78da3187591ae6d0eca08f
jonathafernandes <jonathafernandes@live.com>
2023-04-10 14:22:59 -0300

    criado a função de erro do input

---
+++ js/form-validation.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form");
+const email = document.getElementById("e-mail");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+})
+
+function checkInputEmail(){
+	const emailValue = email.value;
+
+	if(emailValue = ""){
+
+	}
+}
+
+function errorImput(input, message){
+	const fomrItem = input.parenteElement;
+	const textMessage = fomrItem.querySelector("a");
+
+	textMessage.innerText = message;
+
+	fomrItem.className = ""
+}
