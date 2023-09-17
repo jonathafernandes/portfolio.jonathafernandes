c4ed7558a12947e70fa027a0fcf535b32ac77397
Jonatha Fernandes <jonathafernandes@live.com>
2023-09-17 08:35:51 -0300

    feita validação do formulário

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,25 +1,34 @@
 const form = document.getElementById("form");
 const email = document.getElementById("e-mail");
-const message = document.getElementById("message");
+const emailField = document.getElementById('email-message');
+const message = document.getElementById('message');
+const errorMessage = document.getElementById('error-message');
 
-form.addEventListener("submit", (event) => {
-	event.preventDefault();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-})
+    const emailValue = checkInputEmail(email.value);
+    checkInputMessage(emailValue);
+});
 
-function checkInputEmail(){
-	const emailValue = email.value;
-
-	if(emailValue = ""){
-
-	}
+function checkInputEmail(emailValue) {
+    if (emailValue === '') {
+        emailField.innerHTML = 'Por favor, digite seu e-mail!';
+        return false;
+    } else if (emailValue.includes('@')) {
+        return true;
+    } else {
+        emailField.innerHTML = 'Digite um formato de e-mail correto!';
+        return false;
+    }
 }
 
-function errorImput(input, message){
-	const fomrItem = input.parenteElement;
-	const textMessage = fomrItem.querySelector("a");
+function checkInputMessage(emailValue) {
+    const messageValue = message.value;
 
-	textMessage.innerText = message;
-
-	fomrItem.className = "";
+    if (messageValue === '' || emailValue === false) {
+        errorMessage.innerHTML = 'Por favor, digite a mensagem!';
+    } else {
+        location.href = './form-sent.html';
+    }
 }
