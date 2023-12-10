5fbdeb05083d7758859d7f8e244b8bba2aedfa9a
jonathafernandes <jonathafernandes@live.com>
2023-12-10 16:00:47 -0300

    add buttons in projects

--- js/projects-data.js
+++ js/projects-data.js
@@ -24,9 +24,7 @@ async function getSpecificRepositories() {
         const repositoriesList = document.getElementById('repositories-list');
         filteredRepositories.forEach(repo => {
         const listItem = document.createElement('li');
-        const repoLink = document.createElement('a');
-        repoLink.href = repo.html_url;
-        repoLink.innerHTML = `
+        listItem.innerHTML = `
             <div class="repo-title">
                 <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo mr-1 color-fg-muted">
                     <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
@@ -41,8 +39,11 @@ async function getSpecificRepositories() {
                 <span class="repo-language-color"></span>
                 <span>${repo.language}</span>
             </div>
+            <div class="buttons">
+                <a href="${repo.homepage}" target="_blank" class="button">Visite</a>
+                <a href="${repo.html_url}" target="_blank" class="button">Ver no GitHub</a>
+            </div>
         `;
-        listItem.appendChild(repoLink);
         repositoriesList.appendChild(listItem);
         });
     } catch (error) {
