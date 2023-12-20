e0209caf52c3f4338b678fef6e0a3bbeef42e9c5
jonathafernandes <jonathafernandes@live.com>
2023-12-20 13:39:58 -0300

    feat: add function to hide back to top button

--- src/js/button.js
+++ src/js/button.js
@@ -1,9 +1,19 @@
 const btnToTop = document.querySelector(".back-to-top")
 
+btnToTop.style.display = "none";
+
+window.addEventListener("scroll", function() {
+    if (window.pageYOffset > 0) {
+        btnToTop.style.display = "block";
+    } else {
+        btnToTop.style.display = "none";
+    }
+});
+
 btnToTop.addEventListener("click", function () {
     window.scroll({
         top: 0,
         left: 0,
         behavior: 'smooth'
-    })
-})
+    });
+});
