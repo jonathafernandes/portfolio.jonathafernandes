a19a69d37d44f2e9769078a1a4393800e935e82a
jonathafernandes <jonathafernandes@live.com>
2024-05-27 18:59:51 -0300

    create responsive menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
+import { styled } from '@mui/material/styles';
+
 import logo104 from '../../public/logo/logo-jonatha-fernandes-1-04.png';
 
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+
+const LogoLink = styled(Link)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    span: {
+        display: 'none'
+    },
+    [theme.breakpoints.up('md')]: {
+        span: {
+            display: 'initial',
+        }
+    }
+}));
+
+const LogoImg = styled('img')(({ theme }) => ({
+    height: 30,
+    marginRight: theme.spacing(1),
+}));
+
+const MenuButton = styled(IconButton)(({ theme }) => ({
+    [theme.breakpoints.up('md')]: {
+        display: 'none'
+    },
+}));
+
+const NavLinks = styled(Box)(({ theme }) => ({
+    boxShadow: 'none',
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+        display: 'flex',
+        flexGrow: 1,
+        justifyContent: 'flex-end',
+    },
+}));
+
+const LinkItem = styled(Link)(({ theme }) => ({
+    marginRight: theme.spacing(1),
+    padding: '0.5rem',
+    fontFamily: 'Manrope, sans-serif',
+    fontSize: '0.875rem',
+    fontWeight: 600,
+    textTransform: 'uppercase',
+    '&:hover': {
+        backgroundColor: 'var(--bg-color)',
+    },
+}));
+
 const Header: React.FC = () => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <header>
-        <Link to="/">
-            <img
-            src={logo104}
-            alt="Jonatha Fernandes - Desenvolvedor Web"
-            />
-            <span>Jonatha Fernandes</span>
-        </Link>
-        <nav id="navigation">
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">Sobre</Link>
-                </li>
-                <li>
-                    <Link to="/stack">Conhecimento</Link>
-                </li>
-                <li>
-                    <Link to="/projects">Projetos</Link>
-                </li>
-            </ul>
-        </nav>
-        </header>
+        <AppBar position="static" sx={{ backgroundColor: '#fff', padding: '1rem 0' }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <LogoLink to="/">
+                    <LogoImg src={logo104} alt="Jonatha Fernandes - Desenvolvedor Web" />
+                    <span>
+                        Jonatha Fernandes
+                    </span>
+                </LogoLink>
+                <MenuButton onClick={handleMenuOpen}>
+                    <HiOutlineMenuAlt3 color='#212121' />
+                </MenuButton>
+                <NavLinks>
+                    <LinkItem to="/">Home</LinkItem>
+                    <LinkItem to="/about">Sobre</LinkItem>
+                    <LinkItem to="/stack">Conhecimento</LinkItem>
+                    <LinkItem to="/projects">Projetos</LinkItem>
+                </NavLinks>
+                <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                    <MenuItem onClick={handleMenuClose}>
+                        <LinkItem to="/">Home</LinkItem>
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                        <LinkItem to="/about">Sobre</LinkItem>
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                        <LinkItem to="/stack">Conhecimento</LinkItem>
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                        <LinkItem to="/projects">Projetos</LinkItem>
+                    </MenuItem>
+                </Menu>
+            </Toolbar>
+        </AppBar>
     );
 };
 
