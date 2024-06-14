95dc4a143cd58426faabbd46655540a49bcba66d
Jonatha Fernandes <101746582+jonathafernandes@users.noreply.github.com>
2024-06-14 14:20:18 +0000

    feat: remove modal

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import './projects.css'
-import axios from 'axios';
-import Button from '../components/button';
-import Spinner from '../utils/spinner';
+import React, { useEffect, useState } from "react";
+import "./projects.css";
+import axios from "axios";
+import Button from "../components/button";
+import Spinner from "../utils/spinner";
 
-import { FaCircle, FaWindowClose } from 'react-icons/fa';
-import { RiGitRepositoryLine } from 'react-icons/ri';
+import { FaCircle, FaWindowClose } from "react-icons/fa";
+import { RiGitRepositoryLine } from "react-icons/ri";
 
 interface Repository {
     name: string;
@@ -30,56 +30,60 @@ const specificRepositories = [
     "nikel",
     "consult-bible",
     "blog.github.io",
-    "monteiro-esportes-cms"
+    "monteiro-esportes-cms",
 ];
 
-const openModal = (modal: HTMLDialogElement) => {
-    modal.showModal();
-    document.body.classList.add('modal-open');
-}
+// const openModal = (modal: HTMLDialogElement) => {
+//     modal.showModal();
+//     document.body.classList.add("modal-open");
+// };
 
-const closeModal = (modal: HTMLDialogElement) => {
-    modal.close();
-    document.body.classList.remove('modal-open');
-}
+// const closeModal = (modal: HTMLDialogElement) => {
+//     modal.close();
+//     document.body.classList.remove("modal-open");
+// };
 
-const setupModalEvents = (listItem: HTMLLIElement) => {
-    const modal = listItem.querySelector('dialog') as HTMLDialogElement;
-    const openModalButton = listItem.querySelector('.repo-image.open-modal') as HTMLButtonElement;
-    const closeModalButton = listItem.querySelector('.close-modal') as HTMLButtonElement;
+// const setupModalEvents = (listItem: HTMLLIElement) => {
+//     const modal = listItem.querySelector("dialog") as HTMLDialogElement;
+//     const openModalButton = listItem.querySelector(
+//         ".repo-image.open-modal",
+//     ) as HTMLButtonElement;
+//     const closeModalButton = listItem.querySelector(
+//         ".close-modal",
+//     ) as HTMLButtonElement;
 
-    openModalButton.addEventListener('click', () => {
-        openModal(modal);
-    });
+//     openModalButton.addEventListener("click", () => {
+//         openModal(modal);
+//     });
 
-    closeModalButton.addEventListener('click', () => {
-        closeModal(modal);
-    });
+//     closeModalButton.addEventListener("click", () => {
+//         closeModal(modal);
+//     });
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-            closeModal(modal);
-        }
-    });
+//     document.addEventListener("keydown", (event) => {
+//         if (event.key === "Escape") {
+//             closeModal(modal);
+//         }
+//     });
 
-    modal.addEventListener('click', (event) => {
-        if (event.target === modal) {
-            closeModal(modal);
-        }
-    });
-}
+//     modal.addEventListener("click", (event) => {
+//         if (event.target === modal) {
+//             closeModal(modal);
+//         }
+//     });
+// };
 
 const Projects: React.FC = () => {
     const [repositories, setRepositories] = useState<Repository[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
-    const urlBlog = 'https://jonathafernandes.github.io/blog.github.io/';
+    const urlBlog = "https://jonathafernandes.github.io/blog.github.io/";
 
     useEffect(() => {
         const fetchRepositories = async () => {
             try {
-                const username = 'jonathafernandes';
+                const username = "jonathafernandes";
                 const perPage = 100;
                 let allRepositories: Repository[] = [];
 
@@ -87,9 +91,15 @@ const Projects: React.FC = () => {
                     const response = await axios.get(url);
                     allRepositories = allRepositories.concat(response.data);
 
-                    const nextLink = response.headers.link && response.headers.link.split(',').find((link: string | string[]) => link.includes('rel="next"'));
+                    const nextLink =
+                        response.headers.link &&
+                        response.headers.link
+                            .split(",")
+                            .find((link: string | string[]) =>
+                                link.includes('rel="next"'),
+                            );
                     if (nextLink) {
-                        const nextUrl = nextLink.split(';')[0].slice(1, -1);
+                        const nextUrl = nextLink.split(";")[0].slice(1, -1);
                         await fetchData(nextUrl);
                     }
                 };
@@ -97,13 +107,15 @@ const Projects: React.FC = () => {
                 const initialUrl = `https://api.github.com/users/${username}/repos?per_page=${perPage}`;
                 await fetchData(initialUrl);
 
-                const filteredRepositories = allRepositories.filter(repo => specificRepositories.includes(repo.name));
+                const filteredRepositories = allRepositories.filter((repo) =>
+                    specificRepositories.includes(repo.name),
+                );
                 setRepositories(filteredRepositories);
             } catch (error: unknown) {
                 if (error instanceof Error) {
-                    setError('Erro ao obter repositórios: ' + error.message);
+                    setError("Erro ao obter repositórios: " + error.message);
                 } else {
-                    setError('Erro ao obter repositórios.');
+                    setError("Erro ao obter repositórios.");
                 }
             } finally {
                 setLoading(false);
@@ -113,67 +125,102 @@ const Projects: React.FC = () => {
         fetchRepositories();
     }, []);
 
-    useEffect(() => {
-        repositories.forEach(repo => {
-            const listItem = document.getElementById(repo.name) as HTMLLIElement;
-            if (listItem) {
-                setupModalEvents(listItem);
-            }
-        });
-    }, [repositories]);
+    // useEffect(() => {
+    //     repositories.forEach((repo) => {
+    //         const listItem = document.getElementById(
+    //             repo.name,
+    //         ) as HTMLLIElement;
+    //         if (listItem) {
+    //             setupModalEvents(listItem);
+    //         }
+    //     });
+    // }, [repositories]);
 
     return (
         <section className="projects">
             <div className="section-title">
-                <h2>Projetos <span>.</span></h2>
+                <h2>
+                    Projetos <span>.</span>
+                </h2>
             </div>
             <div id="userData">
                 {loading ? (
                     <Spinner />
                 ) : (
                     <>
-                        {error && <p style={{ color: 'red' }}>{error}</p>}
+                        {error && <p style={{ color: "red" }}>{error}</p>}
                         <ul id="repositories-list">
-                            {repositories.map(repo => (
+                            {repositories.map((repo) => (
                                 <li key={repo.name} id={repo.name}>
                                     <div className="repo-title">
                                         <RiGitRepositoryLine />
                                         <h4>{repo.name}</h4>
                                     </div>
                                     <div className="repo-description">
-                                        <span>{repo.name === 'blog.github.io' ? 'Blog pessoal criado com Quartz, Obsidian e GitHub Pages.' : repo.description}</span>
+                                        <span>
+                                            {repo.name === "blog.github.io"
+                                                ? "Blog pessoal criado com Quartz, Obsidian e GitHub Pages."
+                                                : repo.description}
+                                        </span>
                                         <br />
                                         <span className="repo-topics"></span>
-                                        {repo.topics.map(topic => (
-                                            <span key={topic} className='repo-topics'>#{topic}<br /></span>
+                                        {repo.topics.map((topic) => (
+                                            <span
+                                                key={topic}
+                                                className="repo-topics"
+                                            >
+                                                #{topic}
+                                                <br />
+                                            </span>
                                         ))}
                                     </div>
                                     <div className="repo-language">
-                                        <FaCircle color='#514796' size={12} />
+                                        <FaCircle color="#514796" size={12} />
                                         <span>{repo.language}</span>
                                     </div>
                                     <br />
                                     <button className="repo-image open-modal">
-                                        <img src={`/${repo.name}.png`} alt={repo.name} />
+                                        <img
+                                            src={`/${repo.name}.png`}
+                                            alt={repo.name}
+                                        />
                                     </button>
                                     <div className="buttons">
-                                        <Button href={repo.html_url} className="primary-button">
+                                        <Button
+                                            href={repo.html_url}
+                                            className="primary-button"
+                                        >
                                             Ver mais
                                         </Button>
                                         <Button
-                                            href={repo.name === 'blog.github.io' ? urlBlog : repo.homepage}
-                                            className={repo.homepage ? 'primary-button' : 'disabled-button not-allowed'}
+                                            href={
+                                                repo.name === "blog.github.io"
+                                                    ? urlBlog
+                                                    : repo.homepage
+                                            }
+                                            className={
+                                                repo.homepage
+                                                    ? "primary-button"
+                                                    : "disabled-button not-allowed"
+                                            }
                                             disabled={!repo.homepage}
                                         >
-                                            {repo.homepage ? 'Visite' : 'Em breve...'}
+                                            {repo.homepage
+                                                ? "Visite"
+                                                : "Em breve..."}
                                         </Button>
                                     </div>
-                                    <dialog>
+                                    {/* <dialog>
                                         <div className="modal-content">
-                                            <button className="close-modal"><FaWindowClose /></button>
-                                            <img src={`/${repo.name}.png`} alt={repo.name} />
+                                            <button className="close-modal">
+                                                <FaWindowClose />
+                                            </button>
+                                            <img
+                                                src={`/${repo.name}.png`}
+                                                alt={repo.name}
+                                            />
                                         </div>
-                                    </dialog>
+                                    </dialog> */}
                                 </li>
                             ))}
                         </ul>
@@ -182,6 +229,6 @@ const Projects: React.FC = () => {
             </div>
         </section>
     );
-}
+};
 
 export default Projects;
