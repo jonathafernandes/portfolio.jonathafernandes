22ec81ea895317bd6de81f1ea596c67a0898b6f8
Jonatha Fernandes <jonathafernandes@live.com>
2024-08-29 12:46:39 -0300

    refactor tech stack

--- src/pages/stack.tsx
+++ src/pages/stack.tsx
@@ -1,76 +1,7 @@
 import React from 'react';
 import './stack.css';
 import StackCard from '../components/stack-card';
-
-import html5Logo from '/techs/html.svg';
-import css3Logo from '/techs/css.svg';
-import gitLogo from '/techs/git.svg';
-import sassLogo from '/techs/sass.svg';
-import javascriptLogo from '/techs/javascript.svg';
-import bootstrapLogo from '/techs/bootstrap-5-logo-icon.svg';
-import reactLogo from '/techs/react.svg';
-import tailwindcssLogo from '/techs/tailwindcss.svg';
-import typescriptLogo from '/techs/typescript.svg';
-import viteLogo from '/techs/vite.svg';
-import nodeLogo from '/techs/nodejs.svg';
-
-const techStack = [
-    {
-        image: html5Logo,
-        tech: "HTML",
-        description: "Utilizada para criar e estruturar conteúdo na web."
-    },
-    {
-        image: css3Logo,
-        tech: "CSS",
-        description: "Linguagem de estilo usada para descrever a apresentação."
-    },
-    {
-        image: gitLogo,
-        tech: "Git",
-        description: "Controle de versão para gerenciar e rastrear mudanças no código-fonte durante o desenvolvimento de software."
-    },
-    {
-        image: sassLogo,
-        tech: "SASS",
-        description: "Linguagem de extensão do CSS. A sua ideia é adicionar recursos especiais como variáveis, mixins, funções e operações."
-    },
-    {
-        image: javascriptLogo,
-        tech: "JavaScript",
-        description: "Linguagem de alto nível, usada principalmente para criar interatividade em páginas web."
-    },
-    {
-        image: bootstrapLogo,
-        tech: "Bootstrap",
-        description: "Ferramenta para criar interfaces web responsivas."
-    },
-    {
-        image: reactLogo,
-        tech: "React",
-        description: "Biblioteca JavaScript para construção de interfaces de usuário reutilizáveis e componentizadas."
-    },
-    {
-        image: tailwindcssLogo,
-        tech: "Tailwind CSS",
-        description: "Framework CSS que permite criar designs personalizados sem sair do HTML."
-    },
-    {
-        image: typescriptLogo,
-        tech: "TypeScript",
-        description: "Superset de JavaScript que adiciona recursos de tipagem estática para melhorar a produtividade no desenvolvimento."
-    },
-    {
-        image: viteLogo,
-        tech: "Vite",
-        description: "Build tool que visa ser uma alternativa mais rápida para o Webpack."
-    },
-    {
-        image: nodeLogo,
-        tech: "Node.js",
-        description: "Plataforma que permite executar JavaScript no lado do servidor."
-    }
-];
+import techStack from '../data/techStack.json';
 
 const Stack: React.FC = () => {
     return (
