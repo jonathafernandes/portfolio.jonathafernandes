241eb07748e3202405f71590fc6b9e0099be94d9
Jonatha Fernandes <jonathafernandes@live.com>
2025-03-24 15:55:10 -0300

    feat: refactor calculateMonths function and update About section with new job details

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,40 +5,41 @@ import image from '/others/about-IMG_2023.webp';
 import AboutCard from '../components/about-card';
 
 const About: React.FC = () => {
-    const calculateMonths = () => {
-        const startDate = new Date(2024, 6);
+    const calculateMonths = (year: number, month: number) => {
+        const startDate = new Date(year, month);
         const currentDate = new Date();
-        
+
         let totalMonths = (currentDate.getFullYear() - startDate.getFullYear()) * 12;
         totalMonths += currentDate.getMonth() - startDate.getMonth();
-        
+
         const years = Math.floor(totalMonths / 12);
         const months = totalMonths % 12;
-    
+
         const yearsText = years > 0 ? `${years} ano${years > 1 ? 's' : ''}` : '';
         const monthsText = months > 0 ? `${months} mes${months > 1 ? 'es' : ''}` : '';
-    
+
         return `${yearsText} ${monthsText}`.trim();
     };
 
-    const months = calculateMonths();
+    const monthsPcal = calculateMonths(2024, 6);
+    const monthsB4 = calculateMonths(2024, 11);
 
     return (
         <>
             <div className="about">
                 <img className="about-img" src={image} alt="Jonatha Fernandes" />
                 <div className="about-text">
-                <span>Quem sou</span>
-                <h2>Jonatha Fernandes</h2>
-                <h3>Desenvolvedor Web</h3>
-                <br />
-                <p>
-                    Sou apaixonado por tecnologia e sempre me identifiquei muito com a área.
+                    <span>Quem sou</span>
+                    <h2>Jonatha Fernandes</h2>
+                    <h3>Desenvolvedor Web</h3>
                     <br />
-                    Meu primeiro contato com programação foi na faculdade em 2018, onde iniciei o aprendizado em
-                    Python. Também tive contato com as linguagens Java e SQL. No momento tenho focado meus estudos
-                    no ecosistema de <strong>JavaScript</strong> com <strong>React</strong>, <strong>Node.js</strong> e <strong>TypeScript</strong>.
-                </p>
+                    <p>
+                        Sou apaixonado por tecnologia e sempre me identifiquei muito com a área.
+                        <br />
+                        Meu primeiro contato com programação foi na faculdade em 2018, onde iniciei o aprendizado em
+                        Python. Também tive contato com as linguagens Java e SQL. No momento tenho focado meus estudos
+                        no ecosistema de <strong>JavaScript</strong> com <strong>React</strong>, <strong>Node.js</strong> e <strong>TypeScript</strong>.
+                    </p>
                 </div>
             </div>
 
@@ -67,7 +68,7 @@ const About: React.FC = () => {
                     />
                     <br />
                     <AboutCard
-                        date={`${months === '' ? `Julho/2024 — Presente` : `Julho/2024 — Presente · ${months}`}`}
+                        date={`${monthsPcal === '' ? `Julho/2024 — Presente` : `Julho/2024 — Presente · ${monthsPcal}`}`}
                         title='Polícia Civil de Alagoas'
                         description='Desenvolvedor Web Júnior'
                         url="https://github.com/GeinfoPcal"
@@ -75,6 +76,17 @@ const About: React.FC = () => {
                     />
                     <AboutCard
                         tags={['javascript', 'typescript', 'react', 'node.js', 'styled-components', 'next.js']}
+                    />
+                    <br />
+                    <AboutCard
+                        date={`${monthsB4 === '' ? `Dezembro/2024 — Presente` : `Dezembro/2024 — Presente · ${monthsB4}`}`}
+                        title='B4 Comunicação'
+                        description='Head de Desenvolvimento Web'
+                        url="https://www.b4comunicacao.com.br/"
+                        organization="www.b4comunicacao.com.br"
+                    />
+                    <AboutCard
+                        tags={['javascript', 'typescript', 'react', 'prisma', 'tailwind', 'next.js', 'postgresql']}
                     />
                 </div>
             </section>
